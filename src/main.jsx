2576ce177ea53888b3a0bb3ca39c0750a35d5f49
Rohith101coder <rohithvadla07@gmail.com>
2026-09-03 18:06:45 +0530

    mobile app issue fixing

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register(`${import.meta.env.BASE_URL}sw.js`);
+  });
+}
