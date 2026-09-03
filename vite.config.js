3cb897940370486a8da0435dc941012cda347bc6
Rohith101coder <rohithvadla07@gmail.com>
2026-09-03 18:14:03 +0530

    mobile app issue fix3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/carebridge/" : "/",
+  base:
+    process.env.NODE_ENV === "production" && process.env.VERCEL !== "1"
+      ? "/carebridge/"
+      : "/",
 });
